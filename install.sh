0a73379ecfdef27f22fdeccec7b90ba7c03536c7
Andrej Resanovic <andrej.resanovic@gmail.com>
2020-03-28 19:32:13 +0100

    Adding install script
    untested

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+sudo apt install fish -y
+curl -L https://get.oh-my.fish | fish
+echo "tmux" > ~/.config/fish/config.fish
+git clone https://github.com/alyei/configs
+cd configs
+cp tmux.conf ~/.tmux.con
+cp vimrc ~/.vimrc
+mv omf/bundle ~/.config/omf/
+mv omf/channel ~/.config/omf/
+mv omf/theme ~/.config/theme
+omf install
+echo "Done."
