626644be9bd13dc6fc8faf22a19f354ba49987d0
Andrej Resanovic <andrej.resanovic@gmail.com>
2020-03-28 19:33:50 +0100

    Added missing chsh to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo apt install fish -y
+chsh -s /usr/bin/fish
 curl -L https://get.oh-my.fish | fish
 echo "tmux" > ~/.config/fish/config.fish
 git clone https://github.com/alyei/configs
