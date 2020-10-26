dd0da6ed59a87618efc51143d8780f7f452b13cf
Andrej Resanovic <andrej.resanovic@gmail.com>
2020-10-26 16:38:18 +0100

    Improve omf install, add auto vim plugin install, fix fish config path

--- install.sh
+++ install.sh
@@ -21,11 +21,12 @@ git clone https://github.com/Alyei/configs.git
 # Move configs to proper location
 cp configs/tmux.conf ~/.tmux.conf
 cp configs/vimrc ~/.vimrc
-cp configs/config.fish ~/.config/
+cp configs/config.fish ~/.config/fish
+
+# Install vim plugins
+vim +PluginInstall +qall
 
 # Install omf, move configs to folder and install plugins
-curl -L https://get.oh-my.fish | fish
+# Doing this last because it starts a new fish shell
 cp -r configs/omf/ ~/.config
-omf install
-
-echo "Done."
+fish -c "curl -L https://get.oh-my.fish | fish"
