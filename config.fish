b6ccae4b9efd2bc01e0a9d63d554ba467b805bae
Andrej Resanovic <andrej.resanovic@gmail.com>
2020-10-26 16:47:58 +0100

    Fixed bad bat alias, added fd alias

--- config.fish
+++ config.fish
@@ -2,7 +2,8 @@ tmux
 if test -f /home/kali/.autojump/share/autojump/autojump.fish; . /home/kali/.autojump/share/autojump/autojump.fish; end
 
 alias ls="exa"
-alias cat="bat"
+alias cat="batcat"
 alias py="python3"
+alias fd="fdfind"
 
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
